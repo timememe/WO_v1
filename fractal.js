91601551685b0883c3aef37a0d7fbe391d492a89
timememe <113831656+timememe@users.noreply.github.com>
2024-07-13 14:11:30 +0500

    10

--- fractal.js
+++ fractal.js
@@ -7,12 +7,13 @@ canvas.height = window.innerHeight;
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    drawFractal(canvas.width / 2, canvas.height / 2, canvas.width / 4, 5);
+    drawAnimatedFractal();
 });
 
 function drawCircle(x, y, radius) {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.strokeStyle = 'white';
     ctx.stroke();
 }
 
@@ -28,5 +29,21 @@ function drawFractal(x, y, radius, depth) {
     drawFractal(x, y - newRadius, newRadius, depth - 1);
 }
 
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-drawFractal(canvas.width / 2, canvas.height / 2, canvas.width / 4, 5);
+let angle = 0;
+function drawAnimatedFractal() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.rotate(angle);
+    ctx.translate(-canvas.width / 2, -canvas.height / 2);
+
+    drawFractal(canvas.width / 2, canvas.height / 2, canvas.width / 4, 5);
+
+    ctx.restore();
+
+    angle += 0.01;
+    requestAnimationFrame(drawAnimatedFractal);
+}
+
+drawAnimatedFractal(); // Initial call to start the animation
