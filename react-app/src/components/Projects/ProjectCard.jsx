3bf283e4dd874fe66e8c87758f918bb18d3f339f
timememe <thetimememe@gmail.com>
2025-10-23 15:32:34 +0500

    1

--- react-app/src/components/Projects/ProjectCard.jsx
+++ react-app/src/components/Projects/ProjectCard.jsx
@@ -1,12 +1,11 @@
-import { Link } from 'react-router-dom';
 import './ProjectCard.css';
 
 export default function ProjectCard({ project }) {
   return (
-    <Link to={`/project/${project.id}`} className="project-card">
+    <div className="project-card">
       <div className="project-card-image">
-        {project.thumbnail ? (
-          <img src={project.thumbnail} alt={project.title} />
+        {project.image ? (
+          <img src={project.image} alt={project.title} />
         ) : (
           <div className="project-card-placeholder">{project.title[0]}</div>
         )}
@@ -16,6 +15,6 @@ export default function ProjectCard({ project }) {
         <p className="project-card-description">{project.description}</p>
         <span className="project-card-category">{project.category}</span>
       </div>
-    </Link>
+    </div>
   );
 }
